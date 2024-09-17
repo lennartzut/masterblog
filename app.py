46a7f4bdb0e99539213c86a09c92403a85965ad0
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-09-17 12:05:15 +0200

    Add index route and json read

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts=blog_posts)
+
+
+def load_blog_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5001, debug=True)
